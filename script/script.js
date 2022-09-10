9213db8913b66748493f2f555803a243ce5ab70a
daliakhalifa02 <daliakhalifa02@gmail.com>
2022-09-11 02:22:35 +0300

    fetching the urls

--- script/script.js
+++ script/script.js
@@ -5,3 +5,8 @@ async function prediction(){
     var url2="https://api.agify.io?name="+name;
     var url3=" https://api.nationalize.io/?name="+name;
     var url4="https://dog.ceo/api/breeds/image/random";
+    //creating new variables "response" and fetching the urls
+    const response1 = await fetch(url1);
+    const response2 = await fetch(url2);
+    const response3 = await fetch(url3);
+    const response4 = await fetch(url4);
